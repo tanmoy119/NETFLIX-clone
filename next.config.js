2304a2e5175ee9def32937f633d3b95e663e2928
unknown <srroy119@gmail.com>
2022-07-14 23:50:34 +0530

    all commit

--- next.config.js
+++ next.config.js
@@ -12,3 +12,11 @@ module.exports = {
   },
 }
 
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
+
