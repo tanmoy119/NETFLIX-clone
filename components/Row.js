84f3df168eeed6d23fd76468c1d17a16afe153d8
unknown <srroy119@gmail.com>
2022-07-22 16:30:04 +0530

    youtube trailer commit

--- components/Row.js
+++ components/Row.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components';
 import axios from '../public/axios';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer'; 
 
 const baseUrlImg = 'https://image.tmdb.org/t/p/original/'; 
 
 function Row({title, fetchUrl, isLargeRow }) {
     const [movies,setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
     useEffect(()=>{
         async function fetchData(){
@@ -17,6 +20,32 @@ function Row({title, fetchUrl, isLargeRow }) {
         fetchData();
     }, [fetchUrl]);
 
+    const opts = {
+        height:"450",
+        width:"100%",
+        playerVars:{
+            autoplay: 1,
+        },
+    }
+
+    const handeleClick = async (movie)=>{
+        try {
+
+            if(trailerUrl){
+                setTrailerUrl('');
+            }else{
+    
+             const url = await  movieTrailer(movie?.name || movie?.original_name || movie?.original_title || movie?.title, {id: true});
+                     setTrailerUrl(url);
+              
+            }
+            
+        } catch (error) {
+            console.log(error);
+        }
+      
+    }
+
 
   return (
     <Container>
@@ -26,11 +55,12 @@ function Row({title, fetchUrl, isLargeRow }) {
             {
                 movies.map((movie)=>{
                     return(
-                        <img className={`${isLargeRow?'imgo':'imgn'}`} key={movie.id} src={`${baseUrlImg}${isLargeRow?movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+                        <img className={`${isLargeRow?'imgo':'imgn'}`} onClick={() => handeleClick(movie)} key={movie.id} src={`${baseUrlImg}${isLargeRow?movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
                     )
                 })
             }
         </RowPoster>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </Container>
   )
 }
